7f1621ef2fabd9eec3f405f90ada652a339b4563
phyohantun29122021 <6431503128@lamduan.mfu.ac.th>
2023-01-20 09:55:14 +0700

    My First Commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app_flutter/login_screen.dart';
+import 'package:travel_app_flutter/pallete.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Pallete.backgroundColor,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
